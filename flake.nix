d74ee58e3970d27fe77aa7142520076645792e50
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-05 22:37:26 +0545

    chore(flakes): add initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # clang-tools should come before the clang I read it some where
+          # the clang does not matters because you will ditch it for the gcc or g++ I know
+          clang-tools
+          clang
+          gcc
+          cmake
+          # ninja
+          # make
+          gdb
+          # valgrind
+          bear
+        ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        env = {
+          CC = "gcc";
+          CXX = "g++";
+        };
+      };
+    };
+}
