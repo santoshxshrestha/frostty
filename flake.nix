7065a8f3df20fc6bcd5e29aeaf6327b9638267fe
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-05 22:39:59 +0545

    init added deps

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  description = "A Nix-flake-based C/C++ development environment";
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
-  outputs = { self, nixpkgs }:
+  description = "A Nix-flake-based C/C++ development environment for forstty";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in {
+    in
+    {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          # clang-tools should come before the clang I read it some where
-          # the clang does not matters because you will ditch it for the gcc or g++ I know
           clang-tools
           clang
           gcc
@@ -19,6 +21,7 @@
           gdb
           # valgrind
           bear
+          sdl3
         ];
         nativeBuildInputs = [ pkgs.pkg-config ];
         env = {
